1580f706ea57ff232b6e69c5af8e35aebb6d7d4c
msejak <7o.mseJak@gmail.com>
2023-11-08 14:23:37 +0300

    fate: added questions Q: Curry, Debounce, Flat, Shuffle

---
+++ src/components/jsQuestions/Shuffle.jsx
@@ -0,0 +1,11 @@
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i -= 1) {
+    const randIdx = Math.floor(Math.random() * (i + 1));
+
+    [arr[i], arr[randIdx]] = [arr[randIdx], arr[i]];
+  }
+
+  return arr;
+}
+
+console.log(shuffle([1, 2, 3, 4]));
