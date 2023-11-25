27be48ce26998f2cca5526ece18ce5a12604310d
msejak <7o.mseJak@gmail.com>
2023-11-25 16:00:34 +0300

    Added Q: counter

---
+++ src/components/jsQuestions/counter.js
@@ -0,0 +1,24 @@
+const counter = () => {
+  let count = 0;
+
+  return function () {
+    count += 1;
+
+    return count;
+  };
+};
+
+const counter1 = counter();
+const counter2 = counter();
+
+console.log(counter1());
+console.log(counter1());
+console.log(counter1());
+
+console.log(counter2());
+console.log(counter2());
+console.log(counter2());
+
+console.log(counter1());
+
+console.log(counter2());
