561fb575b28901c8ce301a6bbcabb28d6ceae1f0
msejak <7o.mseJak@gmail.com>
2023-12-30 12:05:19 +0300

    feat: added typescript

---
+++ src/components/jstsQuestions/maxSubArray.ts
@@ -0,0 +1,32 @@
+// O(n), O(n)
+
+const maxSubArray = (nums: number[]): number => {
+  const set = new Set<number>();
+  let max: number = 0;
+  let left: number = 0;
+  let right: number = 0;
+
+  while (right < nums.length) {
+    if (set.has(nums[right])) {
+      while (nums[left] !== nums[right]) {
+        set.delete(nums[left]);
+        left += 1;
+      }
+    }
+
+    set.add(nums[right]);
+    max = Math.max(max, set.size);
+    right += 1;
+  }
+
+  return max;
+};
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6
+console.log(maxSubArray([-2, -1])); // 2
+console.log(maxSubArray([1])); // 1
+console.log(maxSubArray([1, 1, 1, 1, 1])); // 1
+console.log(maxSubArray([1, 2, 3, 3, 2, 1])); // 3
+console.log(maxSubArray([1, 2, 3, 4, 5])); // 5
+console.log(maxSubArray([1, 2, 3, 3, 4, 5, 6])); // 4
+console.log(maxSubArray([1, 2, 3, 4, 5, 2, 2])); // 5
