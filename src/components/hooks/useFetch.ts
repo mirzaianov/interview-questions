2cfcc5bc3178e2d7aa277970f5a2f76168dc30fc
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-10-30 15:04:16 +0300

    feat: Add useFetch.ts

---
+++ src/components/hooks/useFetch.ts
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+
+// ! Correct types
+type Data = any;
+
+const useFetch = (
+  url: string,
+): { data: Data | null; isLoading: boolean; error: Error | null } => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        setError(null);
+
+        const response = await fetch(url);
+
+        // Server Error
+        // Check whether the response was successful (status in the range 200-299)
+        if (!response.ok) {
+          throw new Error(`Server error! Status: ${response.status}`);
+        }
+
+        const result = await response.json();
+
+        if (isMounted) {
+          setData(result);
+        }
+
+        // Fetch Error
+        // Check whether there are nwetwork issues
+      } catch (e) {
+        if (isMounted) {
+          setError(e instanceof Error ? e : new Error('An unknown error!'));
+        }
+      } finally {
+        if (isMounted) {
+          setIsLoading(false);
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [url]);
+
+  return { data, isLoading, error };
+};
+
+export default useFetch;
