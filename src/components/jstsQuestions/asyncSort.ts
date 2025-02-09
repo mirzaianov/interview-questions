27941669082016f5bd6326468c8bb0471bcf290c
Riaz Mirzaianov <7o.mseJak@gmail.com>
2025-02-09 11:40:49 +0300

    refactor: Update asyncSort

--- src/components/jstsQuestions/asyncSort.ts
+++ src/components/jstsQuestions/asyncSort.ts
@@ -1,8 +1,8 @@
 const nums: number[] = [5, 7, 1, 3];
 const sortedNums: number[] = [];
 
-const asyncSort = (arr: number[]): Promise<number>[] => {
-  return arr.map((num) => {
+const asyncSort = (arr: number[]) => {
+  arr.forEach((num) => {
     return new Promise<number>((resolve) => {
       setTimeout(() => {
         resolve(sortedNums.push(num));
