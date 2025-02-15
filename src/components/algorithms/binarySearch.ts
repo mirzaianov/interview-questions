c75da8ae1c751ac44ee8a355190e4cdcd529ce62
Riaz Mirzaianov <7o.mseJak@gmail.com>
2025-02-15 10:09:17 +0300

    refactor: binarySearch

--- src/components/algorithms/binarySearch.ts
+++ src/components/algorithms/binarySearch.ts
@@ -1,15 +1,15 @@
-const binarySearch = (array: number[], target: number): number => {
+const binarySearch = (nums: number[], target: number): number => {
   let l = 0;
-  let r: number = array.length - 1;
+  let r: number = nums.length - 1;
 
   while (l <= r) {
     const m: number = l + Math.floor((r - l) / 2);
 
-    if (array[m] === target) return m;
+    if (nums[m] === target) return m;
 
-    if (array[m] < target) l = m + 1;
+    if (nums[m] < target) l = m + 1;
 
-    if (array[m] > target) r = m - 1;
+    if (nums[m] > target) r = m - 1;
   }
 
   return -1;
